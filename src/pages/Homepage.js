a13ed548dcfa3a34039d26ef2c5753ce54af633d
Sophie Zhou <sophiezy@umich.edu>
2025-05-27 23:57:53 +0800

    Add files via upload

---
+++ src/pages/Homepage.js
@@ -0,0 +1,21 @@
+// export default function HomePage() {
+//   return (
+//     <div className="content">
+//       <h1>Welcome to Team Flow</h1>
+//       <p>
+//         Team Flow is a biomedical engineering project team at the University of Michigan building a low-cost medical device to reduce IV infusion misadministrations with a simple light-based tracking system. Our primary partnerships and affiliations are within the University of Michigan and our community partner hospital, the Komfo Anokye Teaching Hospital in Kumasi, Ghana.
+//       </p>
+//       <p>
+//         We are a diverse group of undergraduate and masters students studying a variety of disciplines across engineering, biological sciences, and business. For more information please reach out at <a href="mailto:mheal-team-flow-leads@umich.edu">mheal-team-flow-leads@umich.edu</a>.
+//       </p>
+//     </div>
+//   );
+// }
+
+export default function HomePage() {
+  return (
+    <div>
+      <h2>This is the homepage</h2>
+    </div>
+  );
+}
