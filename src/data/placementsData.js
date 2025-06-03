dcb31d32f71771b4e49b987d98aceb01272e5090
Sophie Zhou <sophiezy@umich.edu>
2025-06-03 12:20:00 +0000

    small tweaks

--- src/data/placementsData.js
+++ src/data/placementsData.js
@@ -3,13 +3,16 @@ const placements = [
   { name: "Stryker", logo: "/placement/stryker.jpg", link: "https://www.stryker.com" },
   { name: "snu", logo: "/placement/snu.png", link: "https://sigmanu.org" },
   { name: "Braun", logo: "/placement/braun.png", link: "https://www.bbraun.com" },
+  { name: "greece", logo: "/placement/greece.png", link: "https://en.wikipedia.org/wiki/Greece" },
   { name: "BCG", logo: "/placement/bcg.png", link: "https://www.bcg.com" },
   { name: "Bond", logo: "/placement/bond.png", link: "https://www.bond-consulting.org/" },
   { name: "pike", logo: "/placement/pike.png", link: "https://www.pikes.org" },
   { name: "BS", logo: "/placement/bs.png", link: "https://www.bostonscientific.com" },
   { name: "png", logo: "/placement/png.png", link: "https://us.pg.com" },
   { name: "GN", logo: "/placement/gn.png", link: "https://www.goodnotes.com" },
-  { name: "greece", logo: "/placement/greece.png", link: "https://en.wikipedia.org/wiki/Greece" },
+  { name: "clown", logo: "/placement/clown.jpeg", link: "https://www.ringling.com" },
+  { name: "ironman", logo: "/placement/ironman.png", link: "https://marvelcinematicuniverse.fandom.com/wiki/Iron_Man" },
+  
   
 ];
 
